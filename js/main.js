aee9c157868f968f7659afa5ffe051a9bdf4766a
Miks Kalnins <MiksKalnins@MaikuMori.com>
2013-01-26 12:22:30 +0200

    Cleanup.

--- js/main.js
+++ js/main.js
@@ -14,174 +14,136 @@ var ready;
 
 var cameraLookahead; // exposed for now
 
-var app = function(){
-    'use strict';
+var app = {
+    init: function () {
+        "use strict";
+        this.controls = false;
 
-    var container, controls,
-        camera, scene, renderer,
-        material,
-        playerPlaceholder;
+        //Halfsize of the window
+        this.halfWidth = window.innerWidth / 2;
+        this.halfHeight = window.innerHeight / 2;
 
-    //Halfsize of the window
-    var halfWidth = window.innerWidth / 2;
-    var halfHeight = window.innerHeight / 2;
+        //Mouse position
+        this.mouseX = this.halfWidth;
+        this.mouseY = -this.halfHeight;
 
-    //Mouse position
-    var mouseX = halfWidth;
-    var mouseY = -halfHeight;
+        //Two PI used in calculations
+        this.tau = Math.PI * 2;
 
-    //Two PI used in calculations
-    var tau = Math.PI * 2;
-
-    cameraLookahead = 500;
-
-    return {
-        init: function(){
-            //Create container div
-            container = document.createElement('div');
-            //Append to the body
-            document.body.appendChild(container);
-
-            //Create the camera
-            //(Field of vision, Aspect ratio, nearest point, farest point)
-            camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 2000);
-
-            camera.position.set(0,1,-cameraLookahead);
-            document.addEventListener( 'keypress', this.onKeyPress, false );
-
-            //Create a new scene
-            scene = new THREE.Scene();
-
-             var ambient = new THREE.AmbientLight( 0x000000 );
-             ambient.position.z = -300;
-             scene.add( ambient );
-
-             var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-             directionalLight.position.set( -10, 1, 30 );
-             directionalLight.position.normalize();
-             scene.add( directionalLight );
-
-             var dLight = new THREE.DirectionalLight( 0xffeedd );
-             dLight.position.set( 1, 0, 1 );
-             dLight.position.normalize();
-             scene.add( dLight );
+        this.cameraLookahead = 500;
 
 
-            //Create the renderer, append to the container
-            //renderer = new THREE.CanvasRenderer();
-            renderer = new THREE.WebGLRenderer({'antialias': true});
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            renderer.domElement.style.position = 'absolute';
-            renderer.domElement.style.top = '0';
+        // Events
+        window.onresize = this.onResize;
 
-            container.appendChild(renderer.domElement);
+        //Create container div
+        this.container = document.createElement('div');
+        //Append to the body
+        document.body.appendChild(this.container);
 
-            //Add the mouse move event
-            document.addEventListener('mousemove', this.mousemove, false);
+        //Create the camera
+        //(Field of vision, Aspect ratio, nearest point, farest point)
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 2000);
 
-            //Add player placeholder
-            playerPlaceholder = new THREE.Mesh(
-                new THREE.SphereGeometry(10,20,20),
-                new THREE.MeshLambertMaterial({ color: 0xff00ff })
-            );
-            playerPlaceholder.position.x = 0;
-            playerPlaceholder.position.z = 1100;
-            scene.add(playerPlaceholder);
+        this.camera.position.set(0, 1, -this.cameraLookahead);
+
+        //Create a new scene
+        this.scene = new THREE.Scene();
+
+        this.ambient = new THREE.AmbientLight(0x000000);
+        this.ambient.position.z = -300;
+        this.scene.add(this.ambient);
+
+        this.directionalLight = new THREE.DirectionalLight(0xffeedd);
+        this.directionalLight.position.set(-10, 1, 30);
+        this.directionalLight.position.normalize();
+        this.scene.add(this.directionalLight);
+
+        this.dLight = new THREE.DirectionalLight(0xffeedd);
+        this.dLight.position.set(1, 0, 1);
+        this.dLight.position.normalize();
+        this.scene.add(this.dLight);
 
 
-            //Draw the bottom grid
-            var geometry = new THREE.Geometry();
-            geometry.vertices.push( new THREE.Vector3( - 1000, 0, 0 ) );
-            geometry.vertices.push( new THREE.Vector3( 1000, 0, 0 ) );
+        //Create the renderer, append to the container
+        //renderer = new THREE.CanvasRenderer();
+        this.renderer = new THREE.WebGLRenderer({'antialias': true});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.domElement.style.position = 'absolute';
+        this.renderer.domElement.style.top = '0';
 
-            material = new THREE.LineBasicMaterial( { color: 0x666666, opacity: 1 } );
+        this.container.appendChild(this.renderer.domElement);
 
-            for ( var i = 0; i <= 40; i ++ ) {
+        //Add player placeholder
+        this.playerPlaceholder = new THREE.Mesh(
+            new THREE.SphereGeometry(10, 20, 20),
+            new THREE.MeshLambertMaterial({ color: 0xff00ff })
+        );
+        this.playerPlaceholder.position.x = 0;
+        this.playerPlaceholder.position.z = 1100;
+        this.scene.add(this.playerPlaceholder);
 
-                var line = new THREE.Line( geometry, material );
-                line.position.y = 0;
-                line.position.z = ( i * 50 ) - 1000;
-                scene.add( line );
 
-                var line = new THREE.Line( geometry, material );
-                line.position.x = ( i * 50 ) - 1000;
-                line.position.y = 0;
-                line.rotation.y = 90 * Math.PI / 180;
-                scene.add( line );
+        //Draw the bottom grid
+        this.geometry = new THREE.Geometry();
+        this.geometry.vertices.push(new THREE.Vector3(-1000, 0, 0));
+        this.geometry.vertices.push(new THREE.Vector3(1000, 0, 0));
 
-            }
+        this.material = new THREE.LineBasicMaterial({
+            color: 0x666666,
+            opacity: 1
+        });
 
-            //Start the animation
-            this.animate();
-        },
-        onKeyPress: function (e) {
+        for (var  i = 40; i >= 0; i--) {
+            var line = new THREE.Line(this.geometry, this.material);
+            line.position.y = 0;
+            line.position.z = (i * 50) - 1000;
+            this.scene.add(line);
 
-            var keyCode = e.which;
+            line = new THREE.Line(this.geometry, this.material);
+            line.position.x = (i * 50) - 1000;
+            line.position.y = 0;
+            line.rotation.y = 90 * Math.PI / 180;
+            this.scene.add(line);
 
-            // backspace
-            if ( keyCode == 8 ) {
-                event.preventDefault();
-            }
-
-            if (keyCode == 119) {
-                console.log('w');
-                playerPlaceholder.position.x += 0.3;
-                camera.lookAt(new THREE.Vector3( camera.position.x, camera.position.y, camera.position.z-5 ));
-            }
-
-            if (keyCode == 97) {
-                playerPlaceholder.position.z += 0.3;
-                console.log('a');
-            }
-
-            if (keyCode == 115) {
-                playerPlaceholder.position.x -= 0.3;
-                console.log('s');
-            }
-
-            if (keyCode == 100) {
-                playerPlaceholder.position.z -= 0.3;
-                console.log('d');
-            }
-
-            //console.log(e);
-        },
-        mousemove: function(e){
-            //X position = current mouse - half width
-            mouseX = e.clientX - halfWidth;
-            //Y position = current mouse - half height
-            mouseY = e.clientY - halfHeight - 250;
-        },
-        radians: function(angle){
-            //Angle supplied in degrees, needs to be converted to radians
-            return angle * (Math.PI / 180);
-        },
-        animate: function(){
-            //cameraLookahead += 3;
-            tick++;
-            //Animate using requestAnimFrame
-            playerPlaceholder.position.set(
-                playerPlaceholder.position.x + Math.sin(tick/70) * 5,
-                playerPlaceholder.position.y,
-                playerPlaceholder.position.z - 3
-            );
-            camera.position.set(
-                playerPlaceholder.position.x,
-                playerPlaceholder.position.y + 300,
-                playerPlaceholder.position.z + cameraLookahead);
-            camera.lookAt(
-                new THREE.Vector3(camera.position.x, 0, camera.position.z - cameraLookahead)
-            );
-
-            requestAnimFrame(app.animate);
-            app.render();
-        },
-        render: function(){
-            renderer.render( scene, camera );
         }
-    }
-}();
 
-ready(function(){
+        //Start the animation
+        this.animate();
+    },
+    onResize: function (e) {
+
+    },
+    radians: function (angle) {
+        //Angle supplied in degrees, needs to be converted to radians
+        return angle * (Math.PI / 180);
+    },
+    animate: function () {
+        //cameraLookahead += 3;
+        tick++;
+        //Animate using requestAnimFrame
+        this.playerPlaceholder.position.set(
+            this.playerPlaceholder.position.x + Math.sin(tick / 70) * 5,
+            this.playerPlaceholder.position.y,
+            this.playerPlaceholder.position.z - 3
+        );
+        this.camera.position.set(
+            this.playerPlaceholder.position.x,
+            this.playerPlaceholder.position.y + 300,
+            this.playerPlaceholder.position.z + this.cameraLookahead);
+        this.camera.lookAt(
+            new THREE.Vector3(this.camera.position.x, 0, this.camera.position.z - this.cameraLookahead)
+        );
+
+        this.requestAnimFrame(app.animate);
+        app.render();
+    },
+    render: function () {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+};
+
+ready(function () {
     app.init();
 });
